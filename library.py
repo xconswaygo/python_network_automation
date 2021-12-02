880957b183b9c35ee0a11c8efea0095a49bcda7e
Evan Wilkerson <ejwilkerson@scheels.com>
2021-12-02 12:43:36 -0600

    test

--- library.py
+++ library.py
@@ -5,6 +5,7 @@ def get_username():
     """ function """
     username_input = str(input('enter in username\n> '))
     while len(str(username_input.strip())) == 0:
+        print('invalid')
         username_input = str(input('enter in username\n> '))
     return str(username_input)
 
@@ -12,6 +13,7 @@ def get_password():
     """ function """
     password_input = str(getpass('enter in password\n> '))
     while len(str(password_input)) == 0:
+        print('invalid')
         password_input = str(getpass('enter in password\n> '))
     return str(password_input)
 
@@ -21,6 +23,7 @@ def get_host_ip_address():
         try:
             ip_address = ipaddress.ip_address(input('enter in an ipv4 address\n> '))
         except ValueError:
+            print('invalid')
             continue
         else:
             return ip_address
@@ -31,6 +34,7 @@ def get_host_mac_address():
         try:
             mac_address = macaddress.MAC(input('enter in a MAC address\n> '))
         except ValueError:
+            print('invalid')
             continue
         else:
-            return mac_address
+            return mac_address.replace('-', ':')
