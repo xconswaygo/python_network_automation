926cb7eecd6f3a93d0616fb7cca3f99ca5075519
Evan Wilkerson <ejwilkerson@scheels.com>
2021-12-03 09:21:52 -0600

    test

--- post-rest-api.py
+++ post-rest-api.py
@@ -18,7 +18,27 @@ loopback_interface = {
         }
     }
 
-def http_post(url, username, password, body):
+def menu():
+    return str(input("""
+please select option:
+
+[1]  show Loopback1000
+[2]  create interface Loopback1000
+[3]  delete interface Loopback1000
+
+> """))
+
+def http_get(url, username, password, params='',):
+    """ function """
+    headers = {
+        'Accept': 'application/yang-data+json',
+        'Content-Type': 'application/yang-data+json',
+        }
+    with httpx.Client(verify=False) as http_client:
+        response = http_client.get(url, headers=headers, params=params, auth=(username, password))
+        return response
+
+def http_post(url, username, password, body,):
     """ function """
     headers = {
         'Accept': 'application/yang-data+json',
@@ -28,22 +48,34 @@ def http_post(url, username, password, body):
         response = http_client.post(url, headers=headers, data=body, auth=(username, password))
         return response
 
+def http_delete(url, username, password,):
+    """ function """
+    headers = {
+        'Accept': 'application/yang-data+json',
+        'Content-Type': 'application/yang-data+json',
+        }
+    with httpx.Client(verify=False) as http_client:
+        response = http_client.delete(url, headers=headers, auth=(username, password))
+        return response
+
 def main():
     """ function """
     username = get_username()
     password = get_password()
     http_host = get_host_ip_address()
     url = f"https://{http_host}:443/restconf/data/Cisco-IOS-XE-native:native/interface"
-    post_loopback_interface_response = http_post(url, username, password, json.dumps(loopback_interface),)
-    print(post_loopback_interface_response)
-    if str(post_loopback_interface_response) == "<Response [201 Created]>":
-        print("success")
-    elif str(post_loopback_interface_response) == "<Response [401 Unauthorized]>":
-        print("unauthorized")
-    elif str(post_loopback_interface_response) == "<Response [409 Conflict]>":
-        print("resource already exists")
-    else:
-        print('error')
+    while True:
+        user_input = menu()
+        if user_input == "1":
+            get_response = http_get(url + "/Loopback=1000", username, password)
+            print(get_response)
+            print(get_response.text)
+        elif user_input == "2":
+            post_response = http_post(url, username, password, json.dumps(loopback_interface),)
+            print(post_response)
+        elif user_input == "3":
+            delete_response = http_delete(url + "/Loopback=1000", username, password)
+            print(delete_response)
 
 if __name__ == '__main__':
     main()
