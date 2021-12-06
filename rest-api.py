cd8ca5470d84bd78796212db508ac6c1cf3b2728
Evan Wilkerson <ejwilkerson@scheels.com>
2021-12-06 09:01:53 -0600

    test

--- rest-api.py
+++ rest-api.py
@@ -43,17 +43,17 @@ def main():
     user_input = get_connection_info()
     url = f"https://{user_input['host']}:443/restconf/data/Cisco-IOS-XE-native:native/interface"
     while True:
-        user_input = menu()
-        if user_input == "1":
+        menu_input = menu()
+        if menu_input == "1":
             get_response = http_get(url + "/Loopback=1000", user_input['username'], user_input['password'], headers)
             print(get_response)
             print(get_response.text)
-        elif user_input == "2":
+        elif menu_input == "2":
             my_addr = get_ip_address_and_mask()
             interface_config['Cisco-IOS-XE-native:Loopback']['ip']['address']['primary']['address'] = my_addr[0]
             interface_config['Cisco-IOS-XE-native:Loopback']['ip']['address']['primary']['mask'] = my_addr[1]
             print(http_post(url, user_input['username'], user_input['password'], headers, json.dumps(interface_config)))
-        elif user_input == "3":
+        elif menu_input == "3":
             print(http_delete(url + "/Loopback=1000", user_input['username'], user_input['password'], headers))
 
 if __name__ == '__main__':
