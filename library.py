d35a13872dac05ca7c1e789b34206c64dba16ea2
Evan Wilkerson <ejwilkerson@scheels.com>
2021-12-09 11:35:24 -0600

    test

--- library.py
+++ library.py
@@ -1,6 +1,7 @@
 from getpass import getpass
 import ipaddress, macaddress, httpx
 
+
 def get_username():
     username_input = str(input('enter in username\n> '))
     while len(str(username_input.strip())) == 0:
@@ -8,12 +9,14 @@ def get_username():
         username_input = str(input('enter in username\n> '))
     return str(username_input)
 
+
 def get_password():
     password_input = str(getpass('enter in password\n> '))
     while len(str(password_input)) == 0:
         print('invalid')
         password_input = str(getpass('enter in password\n> '))
     return str(password_input)
+
 
 def get_host_ip_address():
     while True:
@@ -25,6 +28,7 @@ def get_host_ip_address():
         else:
             return str(ip_address)
 
+
 def get_ip_address_and_mask():
     while True:
         try:
@@ -34,6 +38,7 @@ def get_ip_address_and_mask():
             continue
         else:
             return str(user_input.split('/')[0]),str(user_input.split('/')[1])
+
 
 def get_host_mac_address():
     while True:
@@ -45,17 +50,22 @@ def get_host_mac_address():
         else:
             return str(mac_address).replace('-', ':')
 
-def http_get(url, username, password, headers, params=''):
-    with httpx.Client(verify=False) as http_client:
-        response = http_client.get(url, headers=headers, params=params, auth=(username, password))
-        return response
 
-def http_post(url, username, password, headers, body):
-    with httpx.Client(verify=False) as http_client:
-        response = http_client.post(url, headers=headers, data=body, auth=(username, password))
-        return response
+def http_get(url, username, password, headers, params='',):
+    with httpx.Client(verify=False) as i:
+        return i.get(url, headers=headers, auth=(username, password), params=params,)
 
-def http_delete(url, username, password, headers):
-    with httpx.Client(verify=False) as http_client:
-        response = http_client.delete(url, headers=headers, auth=(username, password))
-        return response
+
+def http_patch(url, username, password, headers, body, params='',):
+    with httpx.Client(verify=False) as i:
+        return i.patch(url, headers=headers, auth=(username, password), data=body, params=params,)
+
+
+def http_post(url, username, password, headers, body, params='',):
+    with httpx.Client(verify=False) as i:
+        return i.post(url, headers=headers, auth=(username, password), data=body, params=params,)
+
+
+def http_delete(url, username, password, headers, params='',):
+    with httpx.Client(verify=False) as i:
+        return i.delete(url, headers=headers, auth=(username, password), params=params,)
